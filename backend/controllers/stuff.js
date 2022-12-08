522c730faec908d0e05de02c52707ab0a1a898b7
vadym-loik <loikfit@gmail.com>
2022-12-08 17:14:38 +0100

    edit files and install multer

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,12 +1,15 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
+  req.body.thing = JSON.parse(req.body.thing);
+  const url = req.protocol + '://' + req.get('host');
   const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
+    title: req.body.thing.title,
+    description: req.body.thing.description,
+    imageUrl: url + '/images/' + req.file.filename,
+    price: req.body.thing.price,
+    userId: req.body.thing.userId,
   });
   thing
     .save()
@@ -37,14 +40,28 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
+  let thing = new Thing({ _id: req.params._id });
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    req.body.thing = JSON.parse(req.body.thing);
+    thing = {
+      _id: req.params.id,
+      title: req.body.thing.title,
+      description: req.body.thing.description,
+      imageUrl: url + '/images/' + req.file.filename,
+      price: req.body.thing.price,
+      userId: req.body.thing.userId,
+    };
+  } else {
+    thing = {
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId,
+    };
+  }
   Thing.updateOne({ _id: req.params.id }, thing)
     .then(() => {
       res.status(201).json({
@@ -60,27 +77,20 @@ exports.modifyThing = (req, res, next) => {
 
 exports.deleteThing = (req, res, next) => {
   Thing.findOne({ _id: req.params.id }).then((thing) => {
-    if (!thing) {
-      return res.status(404).json({
-        error: new Error('Objet non trouvé !'),
-      });
-    }
-    if (thing.userId !== req.auth.userId) {
-      return res.status(401).json({
-        error: new Error('Requête non autorisée !'),
-      });
-    }
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => {
-        res.status(200).json({
-          message: 'Deleted!',
+    const filename = thing.imageUrl.split('/images/')[1];
+    fs.unlink('images/' + filename, () => {
+      Thing.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({
+            message: 'Deleted!',
+          });
+        })
+        .catch((error) => {
+          res.status(400).json({
+            error: error,
+          });
         });
-      })
-      .catch((error) => {
-        res.status(400).json({
-          error: error,
-        });
-      });
+    });
   });
 };
 
